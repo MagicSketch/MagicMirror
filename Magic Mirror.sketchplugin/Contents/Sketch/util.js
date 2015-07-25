d02c3b7e101f37a76b3e0ea1e8deda4f53e3d43a
James Tang <j@jamztang.com>
2015-07-26 01:15:40 +0800

    Refactor

---
+++ Magic Mirror.sketchplugin/Contents/Sketch/util.js
@@ -0,0 +1,24 @@
+var count = function(array) {
+    if (!array) return
+    var count = 0
+    if ( typeof(array.count) == "number") {
+        count = array.count
+    } else if (typeof(array.count) == "function") {
+        count = array.count()
+    } else if (typeof(array.length) == "number") {
+        count = array.length
+    } else if (typeof(array.length) == "function") {
+        count = array.length()
+    } 
+    return count
+}
+
+var map = function(array, callback) {
+    if (!array) return
+    var values = []
+    for (var i = 0; i < count(array); i++) {
+        var value = array[i]
+        values.push(callback(value))
+    }
+    return values
+}
