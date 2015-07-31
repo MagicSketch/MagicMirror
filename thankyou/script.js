ffd70f232ec398b1445dcf40acc6706c08248bd8
James Tang <j@jamztang.com>
2015-08-01 02:26:25 +0800

    Real fix for download

--- thankyou/script.js
+++ thankyou/script.js
@@ -2,6 +2,7 @@ var hello = function() {
 	alert("hello world!")
 }
 
-var download = function() {
-  window.open("http://cl.ly/0U373l3h0f2N/download/Magic%20Mirror%20Package.zip")
+var downloadPressed = function() {
+  window.location.href = "http://cl.ly/0U373l3h0f2N/download/Magic%20Mirror%20Package.zip"
+  // window.location.href = "http://google.com"
 }
