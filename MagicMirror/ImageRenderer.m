585fc3981b19717f5d07616d29d3b07da89ff83c
James Tang <j@jamztang.com>
2016-01-02 00:21:59 +0800

    Add Watermarks

--- MagicMirror/ImageRenderer.m
+++ MagicMirror/ImageRenderer.m
@@ -18,6 +18,7 @@
 @interface ImageRenderer ()
 
 @property (nonatomic, strong) id <MSLayerFlattener> flattener;
+@property (nonatomic) BOOL addWatermarks;
 
 @end
 
@@ -26,6 +27,7 @@
 - (id)init {
     if (self = [super init]) {
         _colorSpaceIdentifier = ImageRendererColorSpaceDeviceRGB;
+        _addWatermarks = YES;
     }
     return self;
 }
@@ -43,6 +45,9 @@
     if ( ! self.disablePerspective) {
         image = [image imageForPath:self.bezierPath scale:self.scale];
     }
+
+    [self addWatermarkOnImageIfNeeded:image];
+
     return image;
 }
 
@@ -56,6 +61,9 @@
     if ( ! self.disablePerspective) {
         image = [image imageForPath:self.bezierPath scale:self.scale];
     }
+
+    [self addWatermarkOnImageIfNeeded:image];
+
     return image;
 }
 
@@ -71,6 +79,37 @@
              [NSColorSpace genericGamma22GrayColorSpace],
              [NSColorSpace adobeRGB1998ColorSpace],
         ][self.colorSpaceIdentifier];
+}
+
+- (void)addWatermarkOnImageIfNeeded:(NSImage *)image {
+    if (self.addWatermarks && self.scale >= 2) {
+        [image lockFocus];
+        CGFloat padding = 10;
+        [self drawWatermarksWithFrame:CGRectMake(padding, padding, image.size.width - padding * 2, image.size.height - padding * 2)];
+        [image unlockFocus];
+    }
+}
+
+- (void)drawWatermarksWithFrame: (NSRect)frame
+{
+
+    //// Text Drawing
+    NSRect textRect = frame;
+    {
+        NSString* textContent = self.scale == 2 ? @"Retina @2x image quality avaliable in Magic Mirror Pro Version" : @"Adaptive + @3x image quality avaliable in Magic Mirror Pro Version" ;
+        NSMutableParagraphStyle* textStyle = NSMutableParagraphStyle.defaultParagraphStyle.mutableCopy;
+        textStyle.alignment = NSCenterTextAlignment;
+
+        NSDictionary* textFontAttributes = @{NSFontAttributeName: [NSFont systemFontOfSize: NSFont.systemFontSize * (CGFloat)self.scale], NSForegroundColorAttributeName: NSColor.greenColor, NSParagraphStyleAttributeName: textStyle};
+
+        NSRect textInset = NSInsetRect(textRect, 10  * (CGFloat)self.scale, 10 * (CGFloat)self.scale);
+        CGFloat textTextHeight = NSHeight([textContent boundingRectWithSize: textInset.size options: NSStringDrawingUsesLineFragmentOrigin attributes: textFontAttributes]);
+        NSRect textTextRect = NSMakeRect(NSMinX(textInset), NSMinY(textInset) + (NSHeight(textInset) - textTextHeight) / 2, NSWidth(textInset), textTextHeight);
+        [NSGraphicsContext saveGraphicsState];
+        NSRectClip(textInset);
+        [textContent drawInRect: NSOffsetRect(textTextRect, 0, 1) withAttributes: textFontAttributes];
+        [NSGraphicsContext restoreGraphicsState];
+    }
 }
 
 @end
