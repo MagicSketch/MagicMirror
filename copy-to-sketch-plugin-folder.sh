4382d7af5d27f4a15f9a3ce5286af95e48e26ddb
James Tang <j@jamztang.com>
2016-02-13 17:58:54 +0800

    Fix SketchToolbox installation

--- copy-to-sketch-plugin-folder.sh
+++ copy-to-sketch-plugin-folder.sh
@@ -1,1 +1,9 @@
-cp -r MagicMirror2.sketchplugin ~/Library/Application\ Support/com.bohemiancoding.sketch3/Plugins/
+#!/bin/bash
+
+# Copy As Normal Install
+# cp -r MagicMirror2.sketchplugin ~/Library/Application\ Support/com.bohemiancoding.sketch3/Plugins/
+
+# Copy As Sketch Toolbox
+rm -rf ~/Library/Application\ Support/com.bohemiancoding.sketch3/Plugins/Magicmirror
+mkdir ~/Library/Application\ Support/com.bohemiancoding.sketch3/Plugins/Magicmirror
+cp -r MagicMirror2.sketchplugin ~/Library/Application\ Support/com.bohemiancoding.sketch3/Plugins/Magicmirror/
